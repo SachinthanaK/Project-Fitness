b47f1250bc0c08a1bdda67ecb3801ad36bf547b7
SachinthanaK <kumaranayakages@gmail.com>
2024-10-19 08:55:41 +0530

    video 6 - 50.00

--- src/components/HomeBanner1/HomeBanner1.tsx
+++ src/components/HomeBanner1/HomeBanner1.tsx
@@ -2,77 +2,114 @@ import React from "react";
 import CircularProgress from "@mui/joy/CircularProgress";
 import { AiOutlineEye } from "react-icons/ai";
 import "./Homebanner1.css";
+import { setDate } from "date-fns";
 
 const HomeBanner1 = () => {
   const [data, setData] = React.useState<any>(null);
+
   const getData = async () => {
-    let temp = [
-      {
-        name: "Calories Intake",
-        value: 2000,
-        unit: "kcal",
-        goal: 2500,
-        goalUnit: "kcal",
-      },
-      {
-        name: "Sleep",
-        value: 8,
-        unit: "hrs",
-        goal: 8,
-        goalUnit: "hrs",
-      },
-      {
-        name: "Steps",
-        value: 50,
-        unit: "steps",
-        goal: 10000,
-        goalUnit: "steps",
-      },
-      {
-        name: "Water",
-        value: 2000,
-        unit: "ml",
-        goal: 3000,
-        goalUnit: "ml",
-      },
-      {
-        name: "Weight",
-        value: 75,
-        unit: "kg",
-        goal: 70,
-        goalUnit: "kg",
-      },
-      {
-        name: "Workout",
-        value: 2,
-        unit: "days",
-        goal: 6,
-        goalUnit: "days",
-      },
-    ];
-    setData(temp);
-    console.log(temp);
+    // let temp = [
+    //   {
+    //     name: "Calories Intake",
+    //     value: 2000,
+    //     unit: "kcal",
+    //     goal: 2500,
+    //     goalUnit: "kcal",
+    //   },
+    //   {
+    //     name: "Sleep",
+    //     value: 8,
+    //     unit: "hrs",
+    //     goal: 8,
+    //     goalUnit: "hrs",
+    //   },
+    //   {
+    //     name: "Steps",
+    //     value: 50,
+    //     unit: "steps",
+    //     goal: 10000,
+    //     goalUnit: "steps",
+    //   },
+    //   {
+    //     name: "Water",
+    //     value: 2000,
+    //     unit: "ml",
+    //     goal: 3000,
+    //     goalUnit: "ml",
+    //   },
+    //   {
+    //     name: "Weight",
+    //     value: 75,
+    //     unit: "kg",
+    //     goal: 70,
+    //     goalUnit: "kg",
+    //   },
+    //   {
+    //     name: "Workout",
+    //     value: 2,
+    //     unit: "days",
+    //     goal: 6,
+    //     goalUnit: "days",
+    //   },
+    // ];
+    // setData(temp);
+    // console.log(temp);
+    /* fetch(process.env.NEXT_PUBLIC_BACKEND_API + "/report/getreport", {
+      method: "GET",
+      credentials: "include",
+    });
+    .then(res=> res.json())
+    .then(data => {
+      console.log(data)
+      if(data.ok){
+        setData (data.data)
+      } else {
+        setData ([])
+      }
+    })
+    .catch(err=>{
+      console.log(err)
+      setDefaultAutoSelectFamily([])
+    })*/
+    try {
+      const response = await fetch(
+        process.env.NEXT_PUBLIC_BACKEND_API + "/report/getreport",
+        {
+          method: "GET",
+          credentials: "include",
+        }
+      );
+      const result = await response.json();
+      if (response.ok) {
+        setData(result.data);
+      } else {
+        setData([]);
+      }
+    } catch (err) {
+      console.log(err);
+      setData([]); // Updated this to handle the error case
+    }
   };
 
   React.useEffect(() => {
     getData();
   }, []);
 
-  function simplifyFraction(
-    numerator: number,
-    denominator: number
-  ): [number, number] {
-    function gcd(a: number, b: number): number {
-      return b === 0 ? a : gcd(b, a % b);
-    }
-    const commonDivisor: number = gcd(numerator, denominator);
+  // function simplifyFraction(
+  //   numerator: number,
+  //   denominator: number
+  // ): [number, number] {
+  //   function gcd(a: number, b: number): number {
+  //     return b === 0 ? a : gcd(b, a % b);
+  //   }
+  //   const commonDivisor: number = gcd(numerator, denominator);
 
-    // Simplify the fraction
-    const simplifiedNumerator: number = numerator / commonDivisor;
-    const simplifiedDenominator: number = denominator / commonDivisor;
+  //   // Simplify the fraction
+  //   const simplifiedNumerator: number = numerator / commonDivisor;
+  //   const simplifiedDenominator: number = denominator / commonDivisor;
 
-    return [simplifiedNumerator, simplifiedDenominator];
-  }
+  //   return [simplifiedNumerator, simplifiedDenominator];
+  // }
 
   return (
     <div className="meters">
@@ -84,13 +121,13 @@ const HomeBanner1 = () => {
                 <div className="card-header-box">
                   <div className="card-header-box-name">{item.name}</div>
                   <div className="card-header-box-value">
-                    {item.value} {item.unit}
+                    {parseInt(item.value)} {item.unit}
                   </div>
                 </div>
                 <div className="card-header-box">
                   <div className="card-header-box-name">Target</div>
                   <div className="card-header-box-value">
-                    {item.goal} {item.goalUnit}
+                    {parseInt(item.goal)} {item.goalUnit}
                   </div>
                 </div>
               </div>
@@ -100,11 +137,14 @@ const HomeBanner1 = () => {
                 variant="solid"
                 value={(item.value / item.goal) * 100}
               >
-                <span className="textincircle">
-                  {simplifyFraction(item.value, item.goal)[0] +
+                <div className="textincircle">
+                  <span>{parseInt(item.value)}</span>
+                  <span className="hrline"></span>
+                  <span>{parseInt(item.goal)}</span>
+                  {/* {simplifyFraction(item.value, item.goal)[0] +
                     " / " +
-                    simplifyFraction(item.value, item.goal)[1]}
-                </span>
+                    simplifyFraction(item.value, item.goal)[1]} */}
+                </div>
               </CircularProgress>
               <button
                 onClick={() => {
